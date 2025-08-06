74a6d2871a0a3cdb61238954a46694c328eefac6
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-08-06 16:55:30 -0500

    Update logout.js

--- logout.js
+++ logout.js
@@ -2,9 +2,9 @@ if (localStorage.getItem("auth") === null) {
     localStorage.setItem("auth", "0");
   }
   if (localStorage.getItem("auth") !== "1") {
-    window.location.href = "https://ryann-4.github.io/Group-Chat-Website-Messaging/index.html";
+    window.location.href = "https://ryann-4.github.io/gcm/index.html";
   }
   function logout() {
     localStorage.setItem("auth", "0");
-    window.location.href = "https://ryann-4.github.io/Group-Chat-Website-Messaging/index.html";
+    window.location.href = "https://ryann-4.github.io/gcm/index.html";
   }
