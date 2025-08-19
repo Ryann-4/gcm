3a2daaca8aca4e286ab2f81b1c258fee2566fbe8
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-08-18 22:09:19 -0500

    Delete files.js

--- files.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('imageInput').addEventListener('change', function () {
-    const fileLabel = document.getElementById('fileLabel');
-    if (this.files.length > 0) {
-        fileLabel.textContent = `Selected: ${this.files[0].name}`;
-    } else {
-        fileLabel.textContent = '';
-    }
-});
